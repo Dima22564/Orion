c077a3f6b23f2759b0b1a071472dd10ad9078628
Dmitriy <drakon-sayron@mail.ru>
2019-10-20 01:21:34 +0500

    (19.10.19)

--- app/js/index.js
+++ app/js/index.js
@@ -1,95 +1,44 @@
 $(document).ready(function () {
-  $('.js-burger-icon').click(function(e){
-    $('.js-burger-menu').slideToggle();
-    if(screen.width < 991){
-      $('.js-main-wrapper').toggleClass('active')
-    }
-  });
-
-  $('.js-main-wrapper').click(function(){
-    if(screen.width < 991){
-      $('.js-burger-menu').slideUp();
-      $(this).removeClass('active');
-
-    }
-  })
-
-  // Slider under  header
-  $('.js-slider-header').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: false,
-    prevArrow: $('.js-btn-prev'),
-    nextArrow: $('.js-btn-next'),
-  })
-  $('.js-second-slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots: false,
-    centerMode: true,
-    variableWidth: true,
-    prevArrow: $('.second-slider-prev-btn'),
-    nextArrow: $('.second-slider-next-btn'),
-    responsive: [
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: false,
-          variableWidth: false,
-
-        }
+  $(".currecy-select")
+    .selectmenu({
+      classes: {
+        "ui-selectmenu-button-closed": "drop-menu-currecy-select",
+        "ui-selectmenu-button-open": "drop-menu-currecy-select",
+        "ui-menu-item-wrapper": "drop-menu",
       },
-    ]
-  })
+      icons: { button: "custom-icon" },
+      transferClasses: true,
+      focus: function (event, ui) {
+        $(ui).addClass('custom');
+      },
+    })
 
-  if(screen.width > 991){
-    $('.js-burger-menu').slideDown();
-  }
-  else{
-    $('.js-burger-menu').hide();
-  }
+    .selectmenu("menuWidget")
 
-  $(window).on('resize', function(){
-    if(screen.width > 991){
-      $('.js-burger-menu').slideDown();
+    function date(parent){
+      $(parent).on('input', function(){
+        let val = $(parent).find('input').val();
+        $(parent).find('.js-out').html(val);
+
+      })
     }
-    else{
-    $('.js-burger-menu').hide();
 
-    }
-  })
+    date('.js-date-1')
+    date('.js-date-2')
 
-  // popup
-  $('.js-popup').click(function(e){
-    if($(e.target).hasClass('popup-body')){
-      closePopup();
-    }
-  })
+    $(".currency-select-1")
+    .selectmenu({
+      classes: {
+        "ui-selectmenu-button-closed": "drop-menu-currecy-select-1",
+        "ui-selectmenu-button-open": "drop-menu-currecy-select-1",
+        "ui-menu-item-wrapper": "drop-menu",
+      },
+      icons: { button: "custom-icon" },
+      transferClasses: true,
+      focus: function (event, ui) {
+        $(ui).addClass('custom');
+      },
+    })
 
-  $('.js-btn-more').click(openPopup);
-
-  $('.js-close').click(closePopup)
-
-  function closePopup(){
-    $('.js-popup').fadeOut();
-  }
-
-  function openPopup(e){
-    e.preventDefault();
-    $('.js-popup').fadeIn();
-  }
-
-  // drop-link
-  $('.js-drop-link').click(function(e){
-    $('.js-drop-down-links').slideToggle();
-    $(this).toggleClass('active')
-  })
-  $('.js-drop-down-links').hover(function(){
-    return 1
-  }, function(){
-    $(this).slideUp();
-    $('.js-drop-link').removeClass('active');
-  })
+    .selectmenu("menuWidget")
 });
--- build/js/index.js
+++ build/js/index.js
@@ -1,86 +1,42 @@
 "use strict";
 
 $(document).ready(function () {
-  $('.js-burger-icon').click(function (e) {
-    $('.js-burger-menu').slideToggle();
-
-    if (screen.width < 991) {
-      $('.js-main-wrapper').toggleClass('active');
+  $(".currecy-select").selectmenu({
+    classes: {
+      "ui-selectmenu-button-closed": "drop-menu-currecy-select",
+      "ui-selectmenu-button-open": "drop-menu-currecy-select",
+      "ui-menu-item-wrapper": "drop-menu"
+    },
+    icons: {
+      button: "custom-icon"
+    },
+    transferClasses: true,
+    focus: function focus(event, ui) {
+      $(ui).addClass('custom');
     }
-  });
-  $('.js-main-wrapper').click(function () {
-    if (screen.width < 991) {
-      $('.js-burger-menu').slideUp();
-      $(this).removeClass('active');
-    }
-  }); // Slider under  header
+  }).selectmenu("menuWidget");
 
-  $('.js-slider-header').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: false,
-    prevArrow: $('.js-btn-prev'),
-    nextArrow: $('.js-btn-next')
-  });
-  $('.js-second-slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots: false,
-    centerMode: true,
-    variableWidth: true,
-    prevArrow: $('.second-slider-prev-btn'),
-    nextArrow: $('.second-slider-next-btn'),
-    responsive: [{
-      breakpoint: 991,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerMode: false,
-        variableWidth: false
-      }
-    }]
-  });
-
-  if (screen.width > 991) {
-    $('.js-burger-menu').slideDown();
-  } else {
-    $('.js-burger-menu').hide();
+  function date(parent) {
+    $(parent).on('input', function () {
+      var val = $(parent).find('input').val();
+      $(parent).find('.js-out').html(val);
+    });
   }
 
-  $(window).on('resize', function () {
-    if (screen.width > 991) {
-      $('.js-burger-menu').slideDown();
-    } else {
-      $('.js-burger-menu').hide();
+  date('.js-date-1');
+  date('.js-date-2');
+  $(".currency-select-1").selectmenu({
+    classes: {
+      "ui-selectmenu-button-closed": "drop-menu-currecy-select-1",
+      "ui-selectmenu-button-open": "drop-menu-currecy-select-1",
+      "ui-menu-item-wrapper": "drop-menu"
+    },
+    icons: {
+      button: "custom-icon"
+    },
+    transferClasses: true,
+    focus: function focus(event, ui) {
+      $(ui).addClass('custom');
     }
-  }); // popup
-
-  $('.js-popup').click(function (e) {
-    if ($(e.target).hasClass('popup-body')) {
-      closePopup();
-    }
-  });
-  $('.js-btn-more').click(openPopup);
-  $('.js-close').click(closePopup);
-
-  function closePopup() {
-    $('.js-popup').fadeOut();
-  }
-
-  function openPopup(e) {
-    e.preventDefault();
-    $('.js-popup').fadeIn();
-  } // drop-link
-
-
-  $('.js-drop-link').click(function (e) {
-    $('.js-drop-down-links').slideToggle();
-    $(this).toggleClass('active');
-  });
-  $('.js-drop-down-links').hover(function () {
-    return 1;
-  }, function () {
-    $(this).slideUp();
-    $('.js-drop-link').removeClass('active');
-  });
+  }).selectmenu("menuWidget");
 });
